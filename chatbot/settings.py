c317d3f716af4b3dc22a5471a4d07ad0e90d5265
VishweshTiwari1323 <vishwesh1323@gmail.com>
2026-07-29 13:19:29 +0530

    Configured project for Vercel serverless deployment

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vpto+@(n69note+)xb%by@__h=dd-do16e*n%i+jvum$d7!pv_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost', '*']
 
 
 # Application definition
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (user-uploaded)
 MEDIA_URL = '/media/'
