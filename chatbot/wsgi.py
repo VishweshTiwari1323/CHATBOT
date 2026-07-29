589532724385e47d13b26608506908e20605c6f3
VishweshTiwari1323 <vishwesh1323@gmail.com>
2026-07-29 14:21:07 +0530

    Remove vercel.json to enable Vercel zero-config Django

--- chatbot/wsgi.py
+++ chatbot/wsgi.py
@@ -1,16 +1,6 @@
 import os
-import sys
-from pathlib import Path
-
-# Adds the middle folder (containing manage.py and APICHAT) to Python path
-BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.append(str(BASE_DIR))
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot.settings')
 
-from django.core.wsgi import get_wsgi_application
-
 application = get_wsgi_application()
-
-# Entry point for Vercel serverless
-app = application
