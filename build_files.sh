af5f7e2bcf4036e4141f0a19665e1b03069e9b93
VishweshTiwari1323 <vishwesh1323@gmail.com>
2026-07-29 17:29:53 +0530

    Add executable build_files.sh for Vercel

---
+++ build_files.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+set -e
+
+echo "=== Installing Dependencies ==="
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+echo "=== Collecting Static Files ==="
+python3 manage.py collectstatic --noinput --clear
+
+echo "=== Build Finished ==="
