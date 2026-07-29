0b1bc89647a38d44031ce4c243ae520ef6b88fcc
VishweshTiwari1323 <vishwesh1323@gmail.com>
2026-07-29 13:27:58 +0530

    Updated settings.py for production deployment

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -9,21 +9,22 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+## Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vpto+@(n69note+)xb%by@__h=dd-do16e*n%i+jvum$d7!pv_'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-vpto+@(n69note+)xb%by@__h=dd-do16e*n%i+jvum$d7!pv_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Set to False in production; turns True automatically if local DEBUG env var is set
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost', '*']
 
